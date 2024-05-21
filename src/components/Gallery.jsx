0de648a739727571759f186ed63d5ccbaa2ccc97
cuatrosoles <ubifon@gmail.com>
2024-05-20 23:54:27 -0300

    Actualizacion2

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,10 +3,12 @@ import PlaceCard from "./PlaceCard";
 import { PlaceCardLoader } from "./loaders";
 import { MainContext } from "../context/MainContext";
 import PhotoAlbum from "react-photo-album";
+import img01 from "../img/imagenes/img01.jpg";
+import img02 from "../img/imagenes/img02.jpg";
 
 const photos = [
-    { src: "/img/imagenes/img01.jpg", width: 800, height: 600 },
-    { src: "/img/imagenes/img02.jpg", width: 1600, height: 900 },
+    { src: img01, width: 800, height: 600 },
+    { src: img02, width: 1600, height: 900 },
   ];
 
 export default function Gallery() {
