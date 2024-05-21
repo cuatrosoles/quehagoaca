e1c9971c174d76390939f6af25740e68a7c8c7fe
cuatrosoles <ubifon@gmail.com>
2024-05-21 00:50:24 -0300

    Actualizacion2

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,24 +1,50 @@
-import { useContext } from "react";
-import PlaceCard from "./PlaceCard";
-import { PlaceCardLoader } from "./loaders";
-import { MainContext } from "../context/MainContext";
-import PhotoAlbum from "react-photo-album";
-import img01 from "../img/imagenes/img01.jpg";
-import img02 from "../img/imagenes/img02.jpg";
-import img03 from "../img/imagenes/img03.jpg";
-import img04 from "../img/imagenes/img04.jpg";
-import img05 from "../img/imagenes/img05.jpg";
-import img06 from "../img/imagenes/img06.jpg";
-import img07 from "../img/imagenes/img07.jpg";
-import img08 from "../img/imagenes/img08.jpg";
-import img09 from "../img/imagenes/img09.jpg";
-import img10 from "../img/imagenes/img10.jpg";
-import img11 from "../img/imagenes/img11.jpg";
-import img12 from "../img/imagenes/img12.jpg";
-import img13 from "../img/imagenes/img13.jpg";
-import img14 from "../img/imagenes/img14.jpg";
-import photos from "../photos.ts";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { PhotoAlbum, RenderPhotoProps } from "react-photo-album";
 
-export default function Gallery() {
-    return <PhotoAlbum layout="masonry" photos={photos} />;
+import Settings, { useSettings } from "./Settings";
+
+function Gallery() {
+  const { photos, layout, columns, targetRowHeight, spacing, padding, width } = useSettings();
+
+  const renderPhoto = React.useCallback(
+    ({ imageProps: { alt, style, ...rest } }: RenderPhotoProps) => (
+      <img
+        alt={alt}
+        style={{
+          ...style,
+          borderRadius: padding > 2 ? "4px" : 0,
+          boxShadow:
+            spacing > 0
+              ? "0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)"
+              : "none",
+          transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+        }}
+        {...rest}
+      />
+    ),
+    [spacing, padding],
+  );
+
+  return (
+    <Box sx={{ width: `${width}%`, mx: "auto" }}>
+      <PhotoAlbum
+        photos={photos}
+        layout={layout}
+        columns={columns}
+        spacing={spacing}
+        padding={padding}
+        targetRowHeight={targetRowHeight}
+        renderPhoto={renderPhoto}
+      />
+    </Box>
+  );
+}
+
+export default function App() {
+  return (
+    <Settings>
+      <Gallery />
+    </Settings>
+  );
 }
