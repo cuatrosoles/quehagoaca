c4caa9ba02698a4fce03432f1bb4135af86bf4fd
cuatrosoles <ubifon@gmail.com>
2024-05-21 18:09:29 -0300

    Actualizacion4

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig ({
   plugins: [react ()],
+
+  build: {
+    lib: {
+      entry: path.resolve (__dirname, 'public/index.html'),
+      name: 'Index',
+      ////fileName: (format) => `my-lib.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 });
