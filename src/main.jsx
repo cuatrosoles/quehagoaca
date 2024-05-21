cd763203896410ef224ca125dca14e6e209af232
cuatrosoles <ubifon@gmail.com>
2024-05-21 11:28:30 -0300

    Actualizacion3

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App'
 import { BrowserRouter as Router } from "react-router-dom"
 import { MainContextProvider } from "./context/MainContext"
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <MainContextProvider>
@@ -15,3 +17,13 @@ ReactDOM.render(
   </MainContextProvider>,
   document.getElementById('root')
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
