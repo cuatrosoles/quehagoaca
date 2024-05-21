a4c80de50148026fa02e7bee828ed0bf53d5b3fd
cuatrosoles <ubifon@gmail.com>
2024-05-20 22:10:50 -0300

    Actualizacion2

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -60,47 +60,47 @@ const Inicio = () => {
             {/* --- */}
 
             {/* Trending in Travel Section */}
-            <div className="container mx-auto px-4 py-10">
+            {/* <div className="container mx-auto px-4 py-10">
                 <h2 className="font-bold text-lg md:text-2xl my-5">
                     Trending in Travel
                 </h2>
-                <div>
+                <div> */}
                     {/* Trending in Travel Toggles */}
-                    <div className="flex text-sm md:text-base space-x-4 md:space-x-8 whitespace-nowrap overflow-x-auto travel_toggle">
+                    {/* <div className="flex text-sm md:text-base space-x-4 md:space-x-8 whitespace-nowrap overflow-x-auto travel_toggle"> */}
                         {/* Places to go toggle */}
-                        <h3 className={`${toggle.toGo ? 'border-black' : 'border-transparent'} font-medium mb-3 border-b-2 pb-1 hover:border-black w-fit cursor-pointer`}
+                        {/* <h3 className={`${toggle.toGo ? 'border-black' : 'border-transparent'} font-medium mb-3 border-b-2 pb-1 hover:border-black w-fit cursor-pointer`}
                             // onClick toggle, all items in the 'toggle' state object is set to false while 'toGo' is true
                             onClick={() => setToggle({toGo: true, toDo: false, toStay: false})}
                         >
                             Places to Go
-                        </h3>
+                        </h3> */}
                         {/* --- */}
 
                         {/* Things to Do toggle */}
-                        <h3 className={`${toggle.toDo ? 'border-black' : 'border-transparent'} font-medium mb-3 border-b-2 pb-1 hover:border-black w-fit cursor-pointer`}
+                        {/* <h3 className={`${toggle.toDo ? 'border-black' : 'border-transparent'} font-medium mb-3 border-b-2 pb-1 hover:border-black w-fit cursor-pointer`}
                             // onClick toggle, all items in the 'toggle' state object is set to false while 'toDo' is true
                             onClick={() => setToggle({toGo: false, toDo: true, toStay: false})}
                         >
                             Things to Do
-                        </h3>
+                        </h3> */}
                         {/* --- */}
 
                         {/* Places to Stay toggle */}
-                        <h3 className={`${toggle.toStay ? 'border-black' : 'border-transparent'} font-medium mb-3 border-b-2 pb-1 hover:border-black w-fit cursor-pointer`}
+                        {/* <h3 className={`${toggle.toStay ? 'border-black' : 'border-transparent'} font-medium mb-3 border-b-2 pb-1 hover:border-black w-fit cursor-pointer`}
                             // onClick toggle, all items in the 'toggle' state object is set to false while 'toStay' is true
                             onClick={() => setToggle({toGo: false, toDo: false, toStay: true})}
                         >
                             Places to Stay
-                        </h3>
+                        </h3> */}
                         {/* --- */}
-                    </div>
+                    {/* </div> */}
                     {/* --- */}
-                    <div>
+                    {/* <div> */}
                         {/* List of Places to Go - Display only if 'toGo'is true */}
-                        {toggle.toGo && (
-                            <div className="grid grid-cols-12">
+                        {/* {toggle.toGo && (
+                            <div className="grid grid-cols-12"> */}
                                 {/* Mapping throughlist of items to render */}
-                                {["Las Vegas Hotels", "Destin Hotels", "Myrtle Beach Hotels", "Gatlinburg Hotels", "Walt Disney World Hotels",
+                                {/* {["Las Vegas Hotels", "Destin Hotels", "Myrtle Beach Hotels", "Gatlinburg Hotels", "Walt Disney World Hotels",
                                 "Orlando Hotels", "Ocean City Hotels", "Panama City Beach Hotels", "Branson Hotels", "Seattle Hotels",
                                 "Turks and Caicos Hotels", "Mazatlan Hotels", "Amalfi Coast Hotels", "Miami Beach Hotels", "Fort Lauderdale Hotels",
                                 "South Padre Island Hotels", "Jackson Hotels", "Yosemite National Park Hotels", "South Lake Tahoe Hotels"]
@@ -109,17 +109,17 @@ const Inicio = () => {
                                             { item }
                                         </a>
                                     ))
-                                }
+                                } */}
                                 {/* --- */}
-                            </div>
-                        )}
+                            {/* </div>
+                        )} */}
                         {/* --- */}
 
                         {/* List of Things to Do - Displays only if 'toDo' is true */}
-                        {toggle.toDo && (
-                            <div className="grid grid-cols-12">
+                        {/*{toggle.toDo && (
+                            <div className="grid grid-cols-12"> */}
                                 {/* Mapping through List of Items to render */}
-                                {["Things to Do in Atlanta", "Things to Do in Austin", "Things to Do in Denver", "Things to Do in Kansas City",
+                                {/* {["Things to Do in Atlanta", "Things to Do in Austin", "Things to Do in Denver", "Things to Do in Kansas City",
                                 "Things to Do in Gatlinburg", "Things to Do in Asheville", "Things to Do in Pigeon Forge", "Things to Do in Philadelphia",
                                 "Things to Do in Santa Barbara", "Things to Do in Myrtle Beach", "Things to Do in Jacksonville", "Things to Do in Colorado",
                                 "Things to Do in Lake Tahoe (California)", "Things to Do in Fredericksburg", "Things to Do in California", "Things to Do in Puerto Rico",
@@ -129,17 +129,17 @@ const Inicio = () => {
                                             { item }
                                         </a>
                                     ))
-                                }
+                                } */}
                                 {/* --- */}
-                            </div>
-                        )}
+                            {/* </div>
+                        )} */}
                         {/* --- */}
 
                         {/* List of Places to Stay - Displays only if 'toStay' is true */}
-                        {toggle.toStay && (
-                            <div className="grid grid-cols-12">
+                        {/* {toggle.toStay && (
+                            <div className="grid grid-cols-12"> */}
                                 {/* Mapping through list of Items to render */}
-                                {["Beaches Turks & Caicos", "Moon Palace Cancun", "Majestic Elegance Costa Mujeres", "Hyatt Ziva Cancun", "Moon Palace Jamaica", "Dreams Punta Cana Resort & Spa",
+                                {/* {["Beaches Turks & Caicos", "Moon Palace Cancun", "Majestic Elegance Costa Mujeres", "Hyatt Ziva Cancun", "Moon Palace Jamaica", "Dreams Punta Cana Resort & Spa",
                                 "Disney's Grand Floridian Resort & Spa", "Majestic Mirage Punta Cana", "Planet Hollywood Cancun", "Barcelo Aruba", "Sheraton Waikiki", "Finest Playa Mujeres", "Hyatt Ziva Cap Cana",
                                 "JW Marriott Marco Island Beach Resort", "Wyndham Alltra Cancun", "Atelier Playa Mujeres", "Hotel Riu Palace Cabo San Lucas", "Grand Hyatt Baha Mar", "The Venetian Resort", "Hyatt Ziva Puerto Vallarta",
                                 "Barcelo Maya Riviera", "Grand Velas Riviera Maya", "Hard Rock Hotel Cancun", "The Ritz-Carlton Orlando, Grande Lakes", "Sandos Caracol Eco Resort", "Hyatt Ziva Los Cabos", "Grand Fiesta Americana Coral Beach",
@@ -150,14 +150,14 @@ const Inicio = () => {
                                             { item }
                                         </a>
                                     ))
-                                }
+                                } */}
                                 {/* --- */}
-                            </div>
-                        )}
+                            {/* </div>
+                        )} */}
                         {/* --- */}
-                    </div>
+                    {/* </div>
                 </div>
-            </div>
+            </div> */}
             {/* --- */}
 
             {/* Footer */}
