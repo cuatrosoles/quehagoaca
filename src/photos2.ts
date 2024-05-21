fb3923d906be39864595323952422fab9ddad5ca
cuatrosoles <ubifon@gmail.com>
2024-05-21 03:10:33 -0300

    Actualizacion2

--- src/photos2.ts
+++ src/photos2.ts
@@ -1,18 +1,18 @@
 
-const img01 = require('img/imagenes/img01.jpg');
-const img02 = require('img/imagenes/img02.jpg');
-const img03 = require('img/imagenes/img03.jpg');
-const img04 = require('img/imagenes/img04.jpg');
-const img05 = require('img/imagenes/img05.jpg');
-const img06 = require('img/imagenes/img06.jpg');
-const img07 = require('img/imagenes/img07.jpg');
-const img08 = require('img/imagenes/img08.jpg');
-const img09 = require('img/imagenes/img09.jpg');
-const img10 = require('img/imagenes/img10.jpg');
-const img11 = require('img/imagenes/img11.jpg');
-const img12 = require('img/imagenes/img12.jpg');
-const img13 = require('img/imagenes/img13.jpg');
-const img14 = require('img/imagenes/img14.jpg');
+const img01 = ('img/imagenes/img01.jpg');
+const img02 = ('img/imagenes/img02.jpg');
+const img03 = ('img/imagenes/img03.jpg');
+const img04 = ('img/imagenes/img04.jpg');
+const img05 = ('img/imagenes/img05.jpg');
+const img06 = ('img/imagenes/img06.jpg');
+const img07 = ('img/imagenes/img07.jpg');
+const img08 = ('img/imagenes/img08.jpg');
+const img09 = ('img/imagenes/img09.jpg');
+const img10 = ('img/imagenes/img10.jpg');
+const img11 = ('img/imagenes/img11.jpg');
+const img12 = ('img/imagenes/img12.jpg');
+const img13 = ('img/imagenes/img13.jpg');
+const img14 = ('img/imagenes/img14.jpg');
 
 
 const breakpoints = [1080, 640, 384, 256, 128, 96, 64, 48];
