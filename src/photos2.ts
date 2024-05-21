dffa247540c9ba26b181970e515331e663335437
cuatrosoles <ubifon@gmail.com>
2024-05-21 03:29:30 -0300

    Actualizacion2

--- src/photos2.ts
+++ src/photos2.ts
@@ -1,18 +1,17 @@
-
-const img01 = ('img/imagenes/img01.jpg');
-const img02 = ('img/imagenes/img02.jpg');
-const img03 = ('img/imagenes/img03.jpg');
-const img04 = ('img/imagenes/img04.jpg');
-const img05 = ('img/imagenes/img05.jpg');
-const img06 = ('img/imagenes/img06.jpg');
-const img07 = ('img/imagenes/img07.jpg');
-const img08 = ('img/imagenes/img08.jpg');
-const img09 = ('img/imagenes/img09.jpg');
-const img10 = ('img/imagenes/img10.jpg');
-const img11 = ('img/imagenes/img11.jpg');
-const img12 = ('img/imagenes/img12.jpg');
-const img13 = ('img/imagenes/img13.jpg');
-const img14 = ('img/imagenes/img14.jpg');
+import img01 from "./img/imagenes/img01.jpg";
+import img02 from "./img/imagenes/img02.jpg";
+import img03 from "./img/imagenes/img03.jpg";
+import img04 from "./img/imagenes/img04.jpg";
+import img05 from "./img/imagenes/img05.jpg";
+import img06 from "./img/imagenes/img06.jpg";
+import img07 from "./img/imagenes/img07.jpg";
+import img08 from "./img/imagenes/img08.jpg";
+import img09 from "./img/imagenes/img09.jpg";
+import img10 from "./img/imagenes/img10.jpg";
+import img11 from "./img/imagenes/img11.jpg";
+import img12 from "./img/imagenes/img12.jpg";
+import img13 from "./img/imagenes/img13.jpg";
+import img14 from "./img/imagenes/img14.jpg";
 
 
 const breakpoints = [1080, 640, 384, 256, 128, 96, 64, 48];
