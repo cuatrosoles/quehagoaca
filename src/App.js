e59ce54eadfd9a8f52abac0cbf55ab1eaa8976d2
cuatrosoles <ubifon@gmail.com>
2024-05-22 19:29:13 -0300

    Actualizacion6

--- src/App.js
+++ src/App.js
@@ -13,34 +13,32 @@ import {PlaceDetails} from './pages/templates';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path={'/'}>
-          <Inicio />
-        </Route>
-        <Route exact path={'/home'}>
-          <Home />
-        </Route>
-        <Route path={'/map'}>
-          <MapView />
-        </Route>
-        <Route exact path={'/restaurants'}>
-          <RestaurantsList />
-        </Route>
-        <Route exact path={'/hotels'}>
-          <HotelsList />
-        </Route>
-        <Route exact path={'/attractions'}>
-          <AttractionsList />
-        </Route>
-        <Route path={'/search'}>
-          <SearchResult />
-        </Route>
-        <Route path={'/:type/:id'}>
-          <PlaceDetails />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route exact path={'/'}>
+        <Inicio />
+      </Route>
+      <Route exact path={'/home'}>
+        <Home />
+      </Route>
+      <Route path={'/map'}>
+        <MapView />
+      </Route>
+      <Route exact path={'/restaurants'}>
+        <RestaurantsList />
+      </Route>
+      <Route exact path={'/hotels'}>
+        <HotelsList />
+      </Route>
+      <Route exact path={'/attractions'}>
+        <AttractionsList />
+      </Route>
+      <Route path={'/search'}>
+        <SearchResult />
+      </Route>
+      <Route path={'/:type/:id'}>
+        <PlaceDetails />
+      </Route>
+    </Switch>
   );
 };
 
