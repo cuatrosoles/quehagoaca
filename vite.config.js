c6c978c13af654b9ae4c77e79c82ca24c9690362
cuatrosoles <ubifon@gmail.com>
2024-05-22 00:46:35 -0300

    Actualizacion5

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig ({
   root: path.join (__dirname, 'public'),
-  base: path.join (__dirname, 'public'),
+  ///base: path.join (__dirname, 'public'),
   plugins: [react ()],
 });
